db3f6a89edf32181c4b6362bfa5280cc83fc6af3
sudikshyakoju <sudikoju@gmail.com>
2021-03-03 12:14:36 +0545

    body mass index

---
+++ pythonlab/bmi.py
@@ -0,0 +1,8 @@
+'''solve each of the following problems using python scripts. Make sure you use appropriate variable
+names and comments. When there is a final answer have python print it to the screen. A person's body mass
+index (BMI) is defined as:(mass/height**2)'''
+
+mass=float(input('enter body mass:'))
+height=float(input('enter height:'))
+BMI=mass/(height**2)
+print(BMI)
--- pythonlab/smallest_possible_number.py
+++ pythonlab/smallest_possible_number.py
@@ -1,22 +1,29 @@
-'''A school decided to replace the desks in three classrooms. Each desksits two students.
+'''A school decided to replace the desks in three classrooms. Each desk sits two students.
 Given the number of students in each class, print the smallest possible number of desks that can be purchased.
 The program should read three integers: the number of students in each of the three classes a,b and respectively.
-In the first test there are three groups. The first group has 20 students and thus, need 10 desks.
-The second group has21 students, so they can get by with no fewer than 11 desks. 11 desk are also enough for the
-third of 22 students. So, we need 32 desk in total.'''
+In the first test there are three groups. The first group has 20 students and thus, need 10 desks.The second
+group has21 students, so they can get by with no fewer than 11 desks. 11 desk are also enough for the third of
+22 students. So, we need 32 desk in total.'''
 
-no. of students class1=int(input("enter the no. of students in first class:"))
-no. of students class2=int(input("enter the no. of students in second class:"))
-no. of students class3=int(input("enter the no. of students in third class:"))
-desk for class1 = (no. of students class1//2)
-print('the required no. of desk for class1 is'{desk for class1})
-desk for class1 = (no. of students class2//2)
-print('the required no. of desk for class2 is'{desk for class2})
-desk for class1 = (no. of students class3//2)
-print('the required no. of desk for class3 is'{desk for class3})
-remain in class1 = no. of students class1 % 2
-print('the remaining no. of desk in class1 is'{remain in class1})
-remain in class2 = no. of students class2 % 2
-print('the remaining no. of desk in class2 is'{remain in class2})
-remain in class3 = no. of students class3 % 2
-print('the remaining no. of desk in class3 is'{remain in class3})
+students class1= int(input('enter the no. of students in first class:'))
+students class2= int(input('enter the no. of students in second class:'))
+students class3= int(input('enter the no. of students in third class:'))
+
+desk class1 = (students class1//2)
+print(f"the required no. of desk for class1 is {desk class1}")
+desk class1 = (students class2//2)
+print(f"the required no. of desk for class2 is {desk class2}")
+desk class1 = (students class3//2)
+print(f"the required no. of desk for class3 is {desk class3}")
+
+remain class1 = (students class1%2)
+print(f"the remaining no. of desk in class1 is {remain class1}")
+remain class2 = (students class2%2)
+print(f"the remaining no. of desk in class2 is {remain class2}")
+remain class3 = (students class3%2)
+print(f"the remaining no. of desk in class3 is {remain class3}")
+
+total=desk class1 + desk class2 + desk class3 + remain class1 + remain class2 +remain class3
+print(total)
+
+
